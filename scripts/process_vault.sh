80958981ba0377acddf568ca9dcf7a59618223d2
donggcao18 <141107494+donggcao18@users.noreply.github.com>
2025-10-16 13:40:54 +0700

    Update process_vault.sh

--- scripts/process_vault.sh
+++ scripts/process_vault.sh
@@ -3,11 +3,11 @@ export HF_HOME="/datadrive5/namlh35/.cache"
 export HF_DATASETS_CACHE="/datadrive5/namlh35/.cache"
 export WANDB_DISABLED="true"
 
-for LANG in C
+for LANG in Ruby
 do
     python3 process_vault.py \
-    --data_path "/data/${LANG}.hf" \
-    --save_dir "/datadrive5/namlh35/CodeGR/data/indexed_data/indexed_data_metatdata_v2" \
+    --data_path "/content/CodeGR/data/${LANG}.hf" \
+    --save_dir "/content/CodeGR/datadrive5/namlh35/CodeGR/data/indexed_data/indexed_data_metatdata_v2" \
     --train_samples -1 \
     --test_samples -1 \
     --index_retrieval_ratio 1 \
