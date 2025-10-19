7129b59a74633579f8c78d854c19c4dce6f05fd2
donggcao18 <caovdongg18@gmail.com>
2025-10-19 14:29:30 +0700

    +

--- DSI-QG/process_data/process.sh
+++ DSI-QG/process_data/process.sh
@@ -5,7 +5,7 @@ for LANG in Ruby
 do
     python3 docT5.py \
     --data_path "/content/data/${LANG}.hf" \
-    --save_dir "/content/CodeGR/data/indexed_data/indexed_data_metatdata_v2" \
+    --save_dir "/content/CodeGR/DSI-QG/process_data" \
     --train_samples -1 \
     --test_samples 1000 \
     --index_retrieval_ratio 1 \
