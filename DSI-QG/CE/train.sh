c209845af5f053dca3808d32a579b857f9ab4b4d
donggcao18 <caovdongg18@gmail.com>
2025-10-19 11:59:37 +0700

    +

--- DSI-QG/CE/train.sh
+++ DSI-QG/CE/train.sh
@@ -6,5 +6,6 @@ model_cfg=microsoft/codebert-base
 python3 train_ranker.py \
 --max_grad_norm 2.0 --warm_ratio 0.1 --seed 42 --ranker_model_cfg ${model_cfg} \
 --train_file /kaggle/input/ruby-ranker/Ruby_ranker_data.json --output_dir ${outdir} --train_steps 10000 \
---train_batch_size 4 --train_num 4 --learning_rate 1e-05 --print_per_step 100 --no_title \
---save_last
+--train_batch_size 4 --train_num 4 --learning_rate 1e-05 --print_per_step 100 \
+--save_last \
+--no_title --over_write
