d260f6f8682abeed1a03cd5375c1a9615a522958
donggcao18 <caovdongg18@gmail.com>
2025-10-20 11:25:53 +0000

    +

--- scripts/semantic_id.sh
+++ scripts/semantic_id.sh
@@ -7,7 +7,7 @@ for LANG in Ruby
 do
     python3 semantic_id.py \
     --data_path "/content/CodeGR/data/${LANG}.hf" \
-    --save_dir "/root/workspace/CodeGR/data/original_indexed_data" \
+    --save_dir "/content/CodeGR/data/indexed_data/indexed_data_metatdata_v2" \
     --train_samples -1 \
     --test_samples -1 \
     --index_retrieval_ratio 1 \
